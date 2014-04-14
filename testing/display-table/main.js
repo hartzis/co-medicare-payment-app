126006fa656c73f2b4e8eb6874a0de992ee293bb
Brian Emil Hartz <brianhartz@gmail.com>
2014-04-13 19:23:38 -0600

    added bar graph and city text

--- testing/display-table/main.js
+++ testing/display-table/main.js
@@ -1,9 +1,13 @@
-var data1;
+var csvData,
+    sumPayments,
+    w,
+    h,
+    barPad;
 
 d3.csv("arv-den.csv", function(csv) {
 
     // MAKE SURE PAYMENTS IS A NUMBER NOT STRING
-    data1 = csv.map(function(d) {
+    csvData = csv.map(function(d) {
         return {
             Provider: d['Provider'],
             Specialty: d['Specialty'],
@@ -13,15 +17,57 @@ d3.csv("arv-den.csv", function(csv) {
         }
     });
 
+    w = 500;
+    h = 350;
+    barPad = 1;
+
+    d3.select("body")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h)
+
     dataLoaded();
 
 });
 
 var dataLoaded = function() {
-    console.log(data1);
-
-    console.log('Payments Sum:', d3.sum(data1, function(d) {
+    console.log(csvData);
+    sumPayments = d3.sum(csvData, function(d) {
         return d.Payments
-    }));
+    })
+    console.log('Payments Sum:', sumPayments);
+
+
+    d3.select('svg')
+        .selectAll('rect .graph')
+        .data(csvData)
+        .enter()
+        .append('rect')
+        .style('fill', '#aaa')
+        .attr("width", w / csvData.length - barPad)
+        .attr('y', function(d) {
+            return 300 - (300 * (d.Payments / sumPayments))
+        })
+        .attr("x", function(d, i) {
+            return i * (w / csvData.length);
+        })
+        .attr('height', function(d) {
+            return (300 * (d.Payments / sumPayments))
+        });
+
+    d3.select('svg')
+        .selectAll('text')
+        .data(csvData)
+        .enter()
+        .append('text')
+        .attr("width", w / csvData.length)
+        .attr('y', '320')
+        .attr("x", function(d, i) {
+            return i * (w / csvData.length);
+        })
+        .attr('height', '35')
+        .text(function(d) {
+            return d.City
+        });
 
 }
