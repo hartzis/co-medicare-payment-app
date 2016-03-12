cb7b9fa02fb996de852ef93acfec3783217f6c21
Brian Emil Hartz <brianhartz@gmail.com>
2016-03-11 18:42:58 -0700

    post to github pages

--- testing/display-table/csv.js
+++
@@ -1,8 +0,0 @@
-var cities_csv = function() {
-    d3.csv("arv-den.csv", function(csv) {
-
-        drawData(csv);
-
-    });
-
-};
--- testing/pieChart/arvada.js
+++
@@ -1,7 +0,0 @@
-var width = 960,
-	height = 500,
-	radius = Math.min(width,height) / 2;
-
-d3.csv("testArvada.csv", function(d) {
-	
-});
